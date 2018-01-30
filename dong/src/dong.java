e8cc00ee038dabb5596d45a35a5599c8c59cec7b
FishSwimmingAllDayLong <huangdongliang1.126.com>
2018-01-30 19:42:08 +0800

    four commit.

--- dong/src/dong.java
+++ dong/src/dong.java
@@ -3,6 +3,7 @@ public class dong {
 	public static void main(String[] args) {
 		System.out.println("second commit!!");
 		System.out.println("Three commit!!");
+		System.out.println("Four commit!!");
 	}
 
 }
