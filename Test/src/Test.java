799388603c366a091e6d3a0c2cb9d9446a04c73b
huangdongliang <huangdongliang1@126.com>
2018-02-08 10:59:55 +0800

    cs

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,16 +1,10 @@
-
 public class Test {
-	private int b;
-	//
-	private int a;
-	
-	//nihao
-	
-	//nihao
-	
+	private int b; 
+	private int a;  
 	public static void main(String[] args) {
 		int a=1;
 		int b=2;
 		System.out.println("Hello World!!"+ (a+b));
+		System.out.println("test1.2 one");
 	}
 }
